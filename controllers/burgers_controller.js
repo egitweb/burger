b8a0f322d475e101047252543081bb573fb1ab52
Eugenio Espinoza <68721930+egitweb@users.noreply.github.com>
2021-01-09 19:09:38 -0600

    Server and handlebar  setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,19 @@
+var mysql = request("mysql");
+
+//Connect mysql
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database: "burger_db"
+});
+
+connection.connect(function(err){
+    if (err){
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+INSERT INTO burger(burger_name, devoured)
+VALUES ("hamburger", false),("cheeseburger", false),("veggieburger", false);
+
+
+----- View Data from database
+SELECT * FROM burger;
